6e274a7d41260937a1b2913a60e2d1df7e236ef6
ApocalypseMess <42765137+ApocalypseMess@users.noreply.github.com>
2020-05-27 14:55:53 +0800

    Update stag.py

--- config/stag.py
+++ config/stag.py
@@ -26,7 +26,7 @@ DATABASES.update(
             'NAME': 'kaoshi',  #  数据库名
             'USER': 'root',  #  用户
             'PASSWORD': '0N9LZr3ksu',  # 数据库密码
-            'HOST': '172.27.0.127', # 数据库 IP
+            'HOST': '172.27.16.59', # 数据库 IP
             'PORT': '3306',
         },
     }
